bb41102865a54d8314c98b0d6b2830a4b1f73aaa
Soundpulse <toby.io@outlook.com>
2019-02-11 12:40:18 +0000

    Created: Working Known pos. model

--- data.py
+++ data.py
@@ -12,10 +12,15 @@ users, books = [], []
 
 
 # Row: users, Column: books, value: ratings
-def fetch_ratings(file, min_rating):
+def fetch_ratings(file, min_rating=0):
+
+    # Ratings can only be integers
+    min_rating = int(min_rating)
+
     path = os.path.join(dir, "csvfiles", file)
     f = open(path)
     reader = csv.reader(f)
+
     for line in reader:
         temp = [x for x in line[0].replace('"', '').split(';')]
         user = int(temp[0])
@@ -35,19 +40,13 @@ def fetch_ratings(file, min_rating):
             data.append(rating)
             i.append(users.index(user))
             j.append(books.index(isbn))
-            print("%d, %d, %d" % (rating, users.index(user), books.index(isbn)))
 
     coo = sp.coo_matrix((data, (i, j)))
-    print(repr(coo))
-    # We return the matrix, the artist dictionary and the amount of users
 
     dictionary = {
-        'matrix': coo,
+        'spr_mtrx': coo,
         'books': books,
         'users': users
     }
 
     return dictionary
-
-
-fetch_ratings("BX-Ratings-Train.csv", 5)
--- main.py
+++ main.py
@@ -1,45 +1,43 @@
 import numpy as np
 from lightfm import LightFM
 from data import fetch_ratings
-from scipy.sparse import hstack
 
 # training and testing data
 
-dfs_train = fetch_ratings("BX-Ratings-Train.csv")
-dfs_test = fetch_ratings("BX-Ratings-Test.csv")
+dfs_train = fetch_ratings("BX-Ratings-Train.csv", 5)
+dfs_test = fetch_ratings("BX-Ratings-Test.csv", 5)
 
 # create model (weighted approximate rank pairwase)
 model = LightFM(loss='warp')
 
 # train model
-model.fit(dfs_train, epochs=30, num_threads=2)
+# model.fit(dfs_train, epochs=30, num_threads=2)
 
 
 def sample_recommendation(model, data, user_ids):
 
-    n_users, n_items = data.shape
+    n_users, n_items = data['spr_mtrx'].shape
+    coo = data['spr_mtrx']
+    csr = coo.tocsr()
 
     for user_id in user_ids:
 
-        # known_positives = dfs_train.indices
-        known_positives = data.tocsr()[user_id].indices
+        # known_positives
+        row = csr.getrow(user_id).toarray()[0].ravel()
+        known_score = row.argsort()
+        known_positives = row[row.argsort()]
+
+        for i in range(1, 5):
+            print("        Book: %s, Index: %s" %
+                  (known_positives[-i], known_score[-i]))
 
         # books our model predicts he'll like
-        scores = model.predict(user_id, np.arange(n_items))
+        # scores = model.predict(user_id, np.arange(n_items))
 
         # rank them in order
-        top_scores = np.argsort(-scores)
+        # top_scores = csr[user_id, np.argsort(-scores)]
 
         # print out the resultsdata['train']
-        print("User %s" % user_id)
-        print("     Known positives:")
-
-        for x in known_positives[:3]:
-            print("         %s" % x)
-        print("     Recommended:")
-
-        for x in top_scores[:3]:
-            print("         %s" % x)
 
 
 sample_recommendation(model, dfs_train, [2])
